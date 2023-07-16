18f6261049b297aa60eb443288c3658a2ad99963
Ghulam shubhani <82954416+GhulamShubhani@users.noreply.github.com>
2023-07-16 19:19:24 +0530

    test

--- Server.js
+++ Server.js
@@ -1,24 +1,23 @@
-
-
-
 import express from "express";
-import dotenv from "dotenv"
-import {errorHandler} from "./middlewere/Errorhandler.js"
-dotenv.config()
-import {connectDB} from "./config/DataBase.js"
-import {sellerRouter} from "./routes/SellerUser.js"
+import dotenv from "dotenv";
+import { errorHandler } from "./middlewere/Errorhandler.js";
+dotenv.config();
+import { connectDB } from "./config/DataBase.js";
+import { sellerRouter } from "./routes/SellerUser.js";
+import cors from "cors";
 
-const PORT = process.env.PORT || 5005
+const PORT = process.env.PORT || 5005;
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/seller",sellerRouter)
+app.use("/seller", sellerRouter);
 
 app.use(errorHandler);
-app.listen(PORT,()=>{
-    console.log(`connect to server ${PORT} `);
-})
+app.listen(PORT, () => {
+  console.log(`Connected to server on port ${PORT}`);
+});
