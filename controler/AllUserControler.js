b4146172829693c4cbd3047c85104b33104291ab
Ghulam shubhani <82954416+GhulamShubhani@users.noreply.github.com>
2023-08-01 19:41:14 +0530

    fix signup bugs

--- controler/AllUserControler.js
+++ controler/AllUserControler.js
@@ -20,11 +20,15 @@ export const AllUserRegistration = asyncHandler(async (request, response) => {
             gender,
             country,
             profilepic,
-            device
+            device,
+            emailCode,
+            OtpId,
+            businessLicense,
         } = request.body;
 
 
-        if (!type || !firstName || !lastName || !email || !password || !mobileNumber || !gender || !device) {
+        if (!type || !firstName || !lastName || !email || !password || !country ||
+            !mobileNumber || !gender || !device || !OtpId || !emailCode) {
             return response.status(400).json({ data: "some field is missing" })
             // throw new Error("backend problem ")
         } else {
@@ -35,6 +39,31 @@ export const AllUserRegistration = asyncHandler(async (request, response) => {
                 if (checkemail.length !== 0) {
                     return response.status(400).json({ message: "email already exist" })
                 }
+                const checkOTP = await EmailOtp.findById(OtpId)
+                console.log(checkOTP.otp, "checkOTP");
+                console.log(emailCode, "emailCode");
+                if (checkOTP.otp == emailCode) {
+                    const createdAtDate = new Date(checkOTP.createdAt);
+                    const currentTime = new Date();
+
+                    // Calculate the difference between the current time and the createdAt time in milliseconds
+                    const timeDifference = currentTime - createdAtDate;
+
+                    // Convert the time difference to minutes
+                    const timeDifferenceInMinutes = timeDifference / (1000 * 60);
+
+                    // Check if the createdAt time is less than 10 minutes from the current time
+                    if (timeDifferenceInMinutes < 10) {
+                        console.log("createdAt time is less than 10 minutes from the current time.");
+                    } else {
+                        return response.status(400).json({ message: "otp time out " })
+                        console.log("createdAt time is greater than or equal to 10 minutes from the current time.");
+                    }
+
+                } else {
+                    return response.status(400).json({ message: "otp not match" })
+                }
+                
                 // Generate a salt
                 // process.env.SALTROUNDS
                 const salt = await bcrypt.genSalt(10);
@@ -55,6 +84,7 @@ export const AllUserRegistration = asyncHandler(async (request, response) => {
                 });
                 const User = await AllUser.create(newAllUser)
                 return response.json({ message: "successfull", data: User })
+                // return response.json({ message: "successfull", data: "User" })
             } catch (err) {
                 console.log("error in regidtration ", err);
                 throw new Error("backend problem ")
@@ -173,16 +203,16 @@ export const AllUserToken = asyncHandler(async (request, response) => {
 export const sendEmailOTP = asyncHandler(async (request, response) => {
     // console.log("sendotp");
     try {
-        const { email,perpous } = request.body;
+        const { email, perpous } = request.body;
         if (!email || !perpous) {
             return response.status(400).json({ data: "some field is missing" })
             // throw new Error("backend problem ")
         } else {
             try {
-                const user = await AllUser.findOne({ email });
-                if (!user) {
-                    return response.status(404).json({ message: "User not found" });
-                }
+                // const user = await AllUser.findOne({ email });
+                // if (!user) {
+                //     return response.status(404).json({ message: "User not found" });
+                // }
 
                 // Generate OTP dynamically (You can use your own OTP generation logic)
                 const otp = generateOTP();
@@ -208,7 +238,7 @@ export const sendEmailOTP = asyncHandler(async (request, response) => {
                     perpous, // Set the appropriate purpose here (e.g., 'signup' or 'forgetpassword')
                     otp: otp,
                 });
-        
+
                 await emailOtpInstance.save();
                 // console.log(mailOptions,"mailoption");
                 // Send the email
@@ -246,7 +276,7 @@ function generateOTP() {
 export const forgetPassword = asyncHandler(async (request, response) => {
     console.log("trrrhhhh");
     try {
-        const { email,otp } = request.body;
+        const { email, otp } = request.body;
         if (!email || !otp) {
             return response.status(400).json({ data: "some field is missing" })
             // throw new Error("backend problem ")
@@ -298,8 +328,8 @@ export const forgetPassword = asyncHandler(async (request, response) => {
 export const ChangePassword = asyncHandler(async (request, response) => {
     console.log("trrr");
     try {
-        const {email,password,newpassword} = request.body
-        
+        const { email, password, newpassword } = request.body
+
 
         if (!email || !password || !newpassword) {
             return response.status(400).json({ data: "some field is missing" })
@@ -310,18 +340,18 @@ export const ChangePassword = asyncHandler(async (request, response) => {
                 if (!user) {
                     return response.status(404).json({ message: "User not found" });
                 }
-                console.log(email,password,newpassword,"0000000",user,request.user);
+                console.log(email, password, newpassword, "0000000", user, request.user);
 
                 const passwordMatch = await bcrypt.compare(password, user.password);
-                console.log("passwordMatch",passwordMatch);
+                console.log("passwordMatch", passwordMatch);
                 if (!passwordMatch) {
                     return response.status(401).json({ message: "Invalid password" });
                 }
 
                 console.log(user, "user");
                 const salt = await bcrypt.genSalt(10)
-                const hashpassword = await bcrypt.hash(newpassword,salt)
-                
+                const hashpassword = await bcrypt.hash(newpassword, salt)
+
                 user.password = hashpassword
                 await user.save();
 
@@ -337,70 +367,70 @@ export const ChangePassword = asyncHandler(async (request, response) => {
     }
 })
 
-export const deleteProfilePic = asyncHandler(async (request,response)=>{
-    try{
-        const {email} = request.body 
-        if(!email){
+export const deleteProfilePic = asyncHandler(async (request, response) => {
+    try {
+        const { email } = request.body
+        if (!email) {
             return response.status(400).json({ data: "some field is missing" })
-        }else{
-            const user = await AllUser.findOne({email})
-            if(!user){
+        } else {
+            const user = await AllUser.findOne({ email })
+            if (!user) {
                 return response.status(404).json({ message: "User not found" });
             }
             user.profilepic = null
             await user.save();
-            return response.json({message:"Image Delete Successfully" , Data:[], status:200})
+            return response.json({ message: "Image Delete Successfully", Data: [], status: 200 })
         }
 
-    }catch(err){ 
+    } catch (err) {
         console.log(err);
     }
 })
 
-export const updateBackGroundProfilePic = asyncHandler(async (request,response)=>{
+export const updateBackGroundProfilePic = asyncHandler(async (request, response) => {
     console.log("up");
-    try{
-        const {email,backgroundProfilePicture} = request.body 
-        console.log("up",email,backgroundProfilePicture);
-        if(!email || !backgroundProfilePicture){
+    try {
+        const { email, backgroundProfilePicture } = request.body
+        console.log("up", email, backgroundProfilePicture);
+        if (!email || !backgroundProfilePicture) {
             return response.status(400).json({ data: "some field is missing" })
-        }else{
-            const user = await AllUser.findOne({email})
-            console.log("upuser",user);
-            if(!user){
+        } else {
+            const user = await AllUser.findOne({ email })
+            console.log("upuser", user);
+            if (!user) {
                 return response.status(404).json({ message: "User not found" });
             }
             user.backgroundProfilePicture = backgroundProfilePicture
             await user.save();
-            return response.json({message:"Back Ground Image Upload Successfully" , Data:[], status:200})
+            return response.json({ message: "Back Ground Image Upload Successfully", Data: [], status: 200 })
         }
 
-    }catch(err){ 
+    } catch (err) {
         console.log(err);
     }
 })
 
-export const deleteBackGroundProfilePic = asyncHandler(async (request,response)=>{
-    try{
-        const {email} = request.body 
-        if(!email){
+export const deleteBackGroundProfilePic = asyncHandler(async (request, response) => {
+    try {
+        const { email } = request.body
+        if (!email) {
             return response.status(400).json({ data: "some field is missing" })
-        }else{
-            const user = await AllUser.findOne({email})
-            if(!user){
+        } else {
+            const user = await AllUser.findOne({ email })
+            if (!user) {
                 return response.status(404).json({ message: "User not found" });
             }
             user.backgroundProfilePicture = null
             await user.save();
-            return response.json({message:"Back Ground Image Delete Successfully" , Data:[], status:200})
+            return response.json({ message: "Back Ground Image Delete Successfully", Data: [], status: 200 })
         }
 
-    }catch(err){ 
+    } catch (err) {
         console.log(err);
     }
 })
 
-export const deleteUser = asyncHandler(async (request,response)=>{
+export const deleteUser = asyncHandler(async (request, response) => {
     try {
         const userId = request.params.userId;
 
@@ -419,22 +449,22 @@ export const deleteUser = asyncHandler(async (request,response)=>{
     }
 });
 
-export const getUserDeta = asyncHandler(async(request,response)=>{
-    try{
-        const {email} = request.body
-        if(!email){
-            return response.json({message:"email field is missing",status:400})
+export const getUserDeta = asyncHandler(async (request, response) => {
+    try {
+        const { email } = request.body
+        if (!email) {
+            return response.json({ message: "email field is missing", status: 400 })
         }
-        const userData = await AllUser.findOne({email})
-        if(!userData){
-            return response.json({message:"wrong email",status:400,data:[]})
+        const userData = await AllUser.findOne({ email })
+        if (!userData) {
+            return response.json({ message: "wrong email", status: 400, data: [] })
         }
-        console.log("user",userData);
+        console.log("user", userData);
 
-       // Return the user details (except password and amount) and the token in the response
-       const userDetails = { ...userData.toObject(), password: undefined, amount: undefined };
-       return response.json({ message: " successful", user: userData });
-    }catch(err){
+        // Return the user details (except password and amount) and the token in the response
+        const userDetails = { ...userData.toObject(), password: undefined, amount: undefined };
+        return response.json({ message: " successful", user: userData });
+    } catch (err) {
         console.log(err);
         return response.status(500).json({ message: 'Backend problem' });
     }
